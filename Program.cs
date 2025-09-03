be3c4892fc99c94397e1da8189a2e9c80d491e2b
Jairo Calcina <j.calcina.v@gmail.com>
2025-09-03 00:39:52 -0400

    feat: add db setup

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PerlaMetroUsersService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var cs = builder.Configuration.GetConnectionString("AppDb");
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseNpgsql(cs));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (DataContext db) => db.Users.ToList());
 
 app.Run();
