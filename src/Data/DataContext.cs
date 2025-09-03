b210461a815222fc5ab155b30b52bdae116fed9e
Jairo Calcina <j.calcina.v@gmail.com>
2025-09-03 00:34:39 -0400

    feat: add data context

---
+++ src/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using PerlaMetroUsersService.Models;
+
+namespace PerlaMetroUsersService.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; } = null!;
+    }
+}
