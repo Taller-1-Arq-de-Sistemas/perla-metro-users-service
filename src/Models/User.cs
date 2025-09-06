72eae7479b52585cfc5e874a75b967354909765a
Jairo Calcina <j.calcina.v@gmail.com>
2025-09-06 07:41:11 -0400

    feat: modify user model

--- src/Models/User.cs
+++ src/Models/User.cs
@@ -1,5 +1,4 @@
 using System.ComponentModel.DataAnnotations;
-using Microsoft.EntityFrameworkCore;
 
 namespace PerlaMetroUsersService.Models
 {
@@ -8,9 +7,12 @@ namespace PerlaMetroUsersService.Models
         [Key]
         public string Id { get; set; } = null!;
         public string Name { get; set; } = null!;
-        public string LastName { get; set; } = null!;
+        public string LastNames { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
-        public bool Status { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? DeletedAt { get; set; } = null;
+        public Role Role { get; set; } = null!;
+        public string RoleId { get; set; } = null!;
     }
 }
