5a8d5927e346c68228f6d98053002122e01b03ce
Jairo Calcina <j.calcina.v@gmail.com>
2025-09-12 01:38:51 -0300

    feat: add mappers for authentication

---
+++ src/Mappers/Auth/AuthResponseMapper.cs
@@ -0,0 +1,18 @@
+using PerlaMetroUsersService.DTOs.Auth;
+using PerlaMetroUsersService.Models;
+
+namespace PerlaMetroUsersService.Mappers.Auth;
+
+internal static class AuthResponseMappers
+{
+    public static RegisterPassengerResponseDto ToRegisterResponse(User u, string roleName, string token)
+        => new()
+        {
+            Id = u.Id,
+            Name = u.Name,
+            LastNames = u.LastNames,
+            Email = u.Email,
+            Role = roleName,
+            Token = token,
+        };
+}
