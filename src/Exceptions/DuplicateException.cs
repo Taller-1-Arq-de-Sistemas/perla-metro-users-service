fe4c78e1755bd03516f251411321d06dc2c23c36
Jairo Calcina <j.calcina.v@gmail.com>
2025-09-12 01:47:20 -0300

    feat: add common exceptions

---
+++ src/Exceptions/DuplicateException.cs
@@ -0,0 +1,13 @@
+namespace PerlaMetroUsersService.Exceptions
+{
+    public class DuplicateException : Exception
+    {
+        public DuplicateException() { }
+
+        public DuplicateException(string? message)
+            : base(message) { }
+
+        public DuplicateException(string? message, Exception? innerException)
+            : base(message, innerException) { }
+    }
+}
