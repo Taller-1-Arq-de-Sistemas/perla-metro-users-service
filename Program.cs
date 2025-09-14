41d2442a43d430f433941a7a549f95abfdf414b8
Jairo Calcina <j.calcina.v@gmail.com>
2025-09-14 03:02:08 -0300

    refactor: change structure file of Program.cs to use app service extensions file

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
-using Microsoft.EntityFrameworkCore;
-using PerlaMetroUsersService.Data;
+using PerlaMetroUsersService.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var cs = builder.Configuration.GetConnectionString("AppDb");
+// Bind to PORT env var if provided (e.g., Render)
+var port = Environment.GetEnvironmentVariable("PORT");
+if (!string.IsNullOrEmpty(port))
+{
+    builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
+}
 
-builder.Services.AddDbContext<DataContext>(options =>
-    options.UseNpgsql(cs));
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddWebApp(builder.Configuration);
 
 var app = builder.Build();
 
-app.MapGet("/", (DataContext db) => db.Users.ToList());
+app.UseWebApp();
+
+AppSeedService.SeedDatabase(app);
 
 app.Run();
