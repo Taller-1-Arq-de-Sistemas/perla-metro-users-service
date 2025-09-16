59005b240b5c2193afed9e92a20243586ee89ad0
Jairo Calcina <j.calcina.v@gmail.com>
2025-09-16 20:36:26 -0300

    docs: add documentation to data management files

--- src/Data/DataContext.cs
+++ src/Data/DataContext.cs
@@ -1,23 +1,42 @@
 using Microsoft.EntityFrameworkCore;
 using PerlaMetroUsersService.Models;
 
-namespace PerlaMetroUsersService.Data
+namespace PerlaMetroUsersService.Data;
+
+/// <summary>
+/// Database context for the application.
+/// </summary>
+public class DataContext : DbContext
 {
-    public class DataContext : DbContext
+    /// <summary>
+    /// Constructor for DataContext.
+    /// </summary>
+    /// <param name="options">The options to be used by a DbContext.</param>
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    /// <summary>
+    /// The Users table in the database.
+    /// </summary>
+    public DbSet<User> Users { get; set; } = null!;
+
+    /// <summary>
+    /// The Roles table in the database.
+    /// </summary>
+    public DbSet<Role> Roles { get; set; } = null!;
+
+    /// <summary>
+    /// Configures the model that was discovered by convention from the entity types
+    /// exposed in DbSet properties on your derived context. The resulting model may be
+    /// cached and re-used for subsequent instances of your derived context.
+    /// </summary>
+    /// <param name="modelBuilder">The builder being used to construct the model for this context.</param>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        base.OnModelCreating(modelBuilder);
 
-        public DbSet<User> Users { get; set; } = null!;
-        public DbSet<Role> Roles { get; set; } = null!;
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            modelBuilder.Entity<User>()
-                .HasIndex(u => u.Email)
-                .IsUnique()
-                .HasFilter("\"DeletedAt\" IS NULL");
-        }
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique()
+            .HasFilter("\"DeletedAt\" IS NULL");
     }
 }
--- src/Data/Seed.cs
+++ src/Data/Seed.cs
@@ -1,82 +1,84 @@
 using System.Text.Json;
 using PerlaMetroUsersService.Models;
 
-namespace PerlaMetroUsersService.Data
+namespace PerlaMetroUsersService.Data;
+
+/// <summary>
+/// Class to seed initial data into the database.
+/// </summary>
+public class Seed
 {
-    public class Seed
+    /// <summary>
+    /// Seed the database with initial data from json files.
+    /// </summary>
+    /// <param name="context">Database context</param>
+    public static void SeedData(DataContext context)
     {
-        /// <summary>
-        /// Seed the database with examples models in the json files if the database is empty.
-        /// </summary>
-        /// <param name="context">Database Context </param>
-        public static void SeedData(DataContext context)
+        ArgumentNullException.ThrowIfNull(context);
+        var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        SeedRoles(context, options);
+        SeedUsers(context, options);
+    }
+
+    /// <summary>
+    /// Seed the database with the roles in the json file, then save changes in the database.
+    /// </summary>
+    /// <param name="context">Database context</param>
+    /// <param name="options">Options to deserialize json</param>
+    private static void SeedRoles(DataContext context, JsonSerializerOptions options)
+    {
+        var result = context.Roles?.Any();
+        if (result is true or null) return;
+
+        var path = Path.Combine(AppContext.BaseDirectory, "Seeders", "RolesData.json");
+        if (!File.Exists(path))
         {
-            ArgumentNullException.ThrowIfNull(context);
-            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
-            SeedRoles(context, options);
-            SeedUsers(context, options);
+            path = Path.Combine(Directory.GetCurrentDirectory(), "src", "Data", "Seeders", "RolesData.json");
+        }
+        var rolesData = File.ReadAllText(path);
+        var rolesList = JsonSerializer.Deserialize<List<Role>>(rolesData, options) ??
+            throw new Exception("RolesData.json is empty");
+
+        context.Roles?.AddRange(rolesList);
+        context.SaveChanges();
+    }
+
+    /// <summary>
+    /// Seed the database with the users in the json file, then save changes in the database.
+    /// </summary>
+    /// <param name="context">Database context</param>
+    /// <param name="options">Options to deserialize json</param>
+    private static void SeedUsers(DataContext context, JsonSerializerOptions options)
+    {
+        var result = context.Users?.Any();
+        if (result is true or null) return;
+
+        var path = Path.Combine(AppContext.BaseDirectory, "Seeders", "UsersData.json");
+        if (!File.Exists(path))
+        {
+            path = Path.Combine(Directory.GetCurrentDirectory(), "src", "Data", "Seeders", "UsersData.json");
+        }
+        var usersData = File.ReadAllText(path);
+        var usersList = JsonSerializer.Deserialize<List<User>>(usersData, options) ??
+            throw new Exception("UsersData.json is empty");
+
+        var rng = new Random();
+        foreach (var user in usersList)
+        {
+            user.Id = Guid.NewGuid();
+            string hashedPassword = BCrypt.Net.BCrypt.HashPassword(user.Password);
+            user.Password = hashedPassword;
+            var daysCreatedRange = rng.Next(-100, 0);
+            user.CreatedAt = DateTime.UtcNow.AddDays(daysCreatedRange);
+            var daysDeletedRange = rng.Next(daysCreatedRange, 365);
+            var deletedChance = rng.Next(1, 10);
+            if (deletedChance <= 2)
+                user.DeletedAt = DateTime.UtcNow.AddDays(daysDeletedRange);
+            else
+                user.DeletedAt = null;
         }
 
-        /// <summary>
-        /// Seed the database with the roles in the json file, then save changes in the database.
-        /// </summary>
-        /// <param name="context">Database context</param>
-        /// <param name="options">Options to deserialize json</param>
-        private static void SeedRoles(DataContext context, JsonSerializerOptions options)
-        {
-            var result = context.Roles?.Any();
-            if (result is true or null) return;
-
-            var path = Path.Combine(AppContext.BaseDirectory, "Seeders", "RolesData.json");
-            if (!File.Exists(path))
-            {
-                path = Path.Combine(Directory.GetCurrentDirectory(), "src", "Data", "Seeders", "RolesData.json");
-            }
-            var rolesData = File.ReadAllText(path);
-            var rolesList = JsonSerializer.Deserialize<List<Role>>(rolesData, options) ??
-                throw new Exception("RolesData.json is empty");
-
-            context.Roles?.AddRange(rolesList);
-            context.SaveChanges();
-        }
-
-        /// <summary>
-        /// Seed the database with the users in the json file, then save changes in the database.
-        /// </summary>
-        /// <param name="context">Database context</param>
-        /// <param name="options">Options to deserialize json</param>
-        private static void SeedUsers(DataContext context, JsonSerializerOptions options)
-        {
-            var result = context.Users?.Any();
-            if (result is true or null) return;
-
-            var path = Path.Combine(AppContext.BaseDirectory, "Seeders", "UsersData.json");
-            if (!File.Exists(path))
-            {
-                path = Path.Combine(Directory.GetCurrentDirectory(), "src", "Data", "Seeders", "UsersData.json");
-            }
-            var usersData = File.ReadAllText(path);
-            var usersList = JsonSerializer.Deserialize<List<User>>(usersData, options) ??
-                throw new Exception("UsersData.json is empty");
-
-            var rng = new Random();
-            foreach (var user in usersList)
-            {
-                user.Id = Guid.NewGuid();
-                string hashedPassword = BCrypt.Net.BCrypt.HashPassword(user.Password);
-                user.Password = hashedPassword;
-                var daysCreatedRange = rng.Next(-100, 0);
-                user.CreatedAt = DateTime.UtcNow.AddDays(daysCreatedRange);
-                var daysDeletedRange = rng.Next(daysCreatedRange, 365);
-                var deletedChance = rng.Next(1, 10);
-                if (deletedChance <= 2)
-                    user.DeletedAt = DateTime.UtcNow.AddDays(daysDeletedRange);
-                else
-                    user.DeletedAt = null;
-            }
-
-            context.Users?.AddRange(usersList);
-            context.SaveChanges();
-        }
+        context.Users?.AddRange(usersList);
+        context.SaveChanges();
     }
 }
